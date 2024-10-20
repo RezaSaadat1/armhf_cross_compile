d27f81077cca294074c40e8a70857b68ea87356a
Reza Sa'adat <onlysaadat@gmail.com>
2024-10-20 17:52:42 +0330

    removing rust installer from docker

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,13 +1,1 @@
 #!/bin/bash
-
-# Install Rust using rustup-init with the -y flag to automate the installation
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-
-# Add Rust to the current shell session
-source $HOME/.cargo/env
-
-# Add the armv7 target
-rustup target add armv7-unknown-linux-gnueabihf
-
-# Install cross and cargo-make
-cargo install cross cargo-make
